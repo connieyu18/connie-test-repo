d839667f439cc14e3f544a3b4856d3649960a08c
Connie Yu <connieyu18@yahoo.com>
2021-06-17 03:00:17 -0700

    revamped security bug fix related to sending messages that belongs to senderID and recipientID only

--- server/routes/api/messages.js
+++ server/routes/api/messages.js
@@ -13,9 +13,37 @@ router.post("/", async (req, res, next) => {
 
     // if we already know conversation id, we can save time and just add it to message and return
     if (conversationId) {
-      const message = await Message.create({ senderId, text, conversationId });
-      return res.json({ message, sender });
+      // check that conversationId belongs to the senderId + recipientId
+
+      // get conversation by id
+      let conversation = await Conversation.findConversationById(
+        conversationId
+      );
+
+      // conversation id is invalid
+      if (!conversation) {
+        return res.sendStatus(404);
+      }
+
+      // sender and recipient are indeed part of the conversation
+      if (
+        (conversation.user1Id == senderId &&
+          conversation.user2Id == recipientId) ||
+        (conversation.user1Id == recipientId &&
+          conversation.user2Id == senderId)
+      ) {
+        const message = await Message.create({
+          senderId,
+          text,
+          conversationId,
+        });
+        return res.json({ message, sender });
+      }
+
+      // conversation id is not valid for given senderId and recipientId
+      return res.sendStatus(400);
     }
+
     // if we don't have conversation id, find a conversation to make sure it doesn't already exist
     let conversation = await Conversation.findConversation(
       senderId,
@@ -31,14 +59,14 @@ router.post("/", async (req, res, next) => {
       if (onlineUsers.includes(sender.id)) {
         sender.online = true;
       }
-    } else {
-      const message = await Message.create({
-        senderId,
-        text,
-        conversationId: conversation.id,
-      });
-      res.json({ message, sender });
     }
+    const message = await Message.create({
+      senderId,
+      text,
+      conversationId: conversation.id,
+    });
+    res.json({ message, sender });
+    return;
   } catch (error) {
     next(error);
   }
