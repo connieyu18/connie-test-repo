1a1a156e8bcefd914ac3416dcc53c7d9bce2cfc2
Connie Yu <connieyu18@yahoo.com>
2021-06-16 17:42:52 -0700

    fixed sending messages bug

--- client/src/store/utils/thunkCreators.js
+++ client/src/store/utils/thunkCreators.js
@@ -78,8 +78,13 @@ export const fetchConversations = () => async (dispatch) => {
   }
 };
 
-const saveMessage = async (body) => {
-  const { data } = await axios.post("/api/messages", body);
+const saveMessage = (body) => {
+  const data = axios
+    .post("/api/messages", body)
+    .then((response) => response.data)
+    .then((data) => {
+      return data;
+    });
   return data;
 };
 
@@ -93,9 +98,9 @@ const sendMessage = (data, body) => {
 
 // message format to send: {recipientId, text, conversationId}
 // conversationId will be set to null if its a brand new conversation
-export const postMessage = (body) => (dispatch) => {
+export const postMessage = (body) => async (dispatch) => {
   try {
-    const data = saveMessage(body);
+    const data = await saveMessage(body);
 
     if (!body.conversationId) {
       dispatch(addConversation(body.recipientId, data.message));
