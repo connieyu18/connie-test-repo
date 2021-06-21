8e27a271bcaa975f5bedb5a4cfc1947a2852cf14
Connie Yu <connieyu18@yahoo.com>
2021-06-21 01:18:48 -0700

    designed for the login and signup pages, and make it mobile responsive

--- client/src/Login.js
+++ client/src/Login.js
@@ -7,12 +7,77 @@ import {
   Typography,
   Button,
   FormControl,
+  Hidden,
   TextField,
+  makeStyles,
 } from "@material-ui/core";
 import { login } from "./store/utils/thunkCreators";
+import bgImage from "./assets/img/bg-img.png";
+import chatIcon from "./assets/img/bubble.svg";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    height: "100vh",
+  },
+  image: {
+    backgroundImage: `linear-gradient(to bottom,rgb(58, 141, 255,0.85), rgb(134, 185, 255,0.85)), url(${bgImage})`,
+    width: "100%",
+    height: "100vh",
+    backgroundSize: "cover",
+    color: "white",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+  },
+  headerText: {
+    marginBottom: "15px",
+    fontWeight: 600,
+  },
+  heroText: {
+    color: "#D8D8D8",
+    fontWeight: 400,
+  },
+  chatIcon: {
+    marginBottom: 30,
+  },
+  topHeader: {
+    alignItems: "center",
+    display: "flex",
+    justifyContent: "flex-end",
+    margin: "5%",
+  },
+  form: {
+    width: "60%",
+    maxWidth: "400px",
+    margin: "0px auto",
+    marginTop: "10rem",
+  },
+  formControl: {
+    width: "100%",
+  },
+  customButton: {
+    borderRadius: "3px",
+    boxShadow: "0px 2px 12px rgba(74, 106, 149, .2)",
+    fontSize: "0.9em",
+  },
+  topHeaderBtn: {
+    backgroundColor: "white",
+    color: "#3A8DFF",
+    marginLeft: "20px",
+    fontWeight: "400",
+  },
+  CTABtn: { display: "flex", margin: "40px auto", padding: "3% 15%" },
+  grayText: {
+    color: "#B0B0B0",
+    fontSize: "0.9em",
+  },
+}));
 
 const Login = (props) => {
+  const classes = useStyles();
   const history = useHistory();
+  console.log("login!", props);
   const { user, login } = props;
 
   const handleLogin = async (event) => {
@@ -28,40 +93,93 @@ const Login = (props) => {
   }
 
   return (
-    <Grid container justify="center">
-      <Box>
-        <Grid container item>
-          <Typography>Need to register?</Typography>
-          <Button onClick={() => history.push("/register")}>Register</Button>
+    <Grid container className={classes.container}>
+      <Hidden only="xs">
+        <Grid item xs={false} sm={8} md={5} className={classes.image}>
+          <Box>
+            <img className={classes.chatIcon} src={chatIcon} alt="chat-icon" />
+            <Typography className={classes.heroText}>
+              Converse with anyone
+            </Typography>
+            <Typography className={classes.heroText}>
+              with any language
+            </Typography>
+          </Box>
         </Grid>
-        <form onSubmit={handleLogin}>
-          <Grid>
-            <Grid>
-              <FormControl margin="normal" required>
-                <TextField
-                  aria-label="username"
-                  label="Username"
-                  name="username"
-                  type="text"
-                />
-              </FormControl>
-            </Grid>
-            <FormControl margin="normal" required>
-              <TextField
-                label="password"
-                aria-label="password"
-                type="password"
-                name="password"
-              />
-            </FormControl>
-            <Grid>
-              <Button type="submit" variant="contained" size="large">
-                Login
-              </Button>
-            </Grid>
+      </Hidden>
+      <Grid item xs={12} sm={4} md={7}>
+        <Box>
+          <Grid className={classes.topHeader}>
+            <Typography className={classes.grayText}>
+              Don't have an account?
+            </Typography>
+            <Button
+              onClick={() => history.push("/register")}
+              className={`${classes.customButton} ${classes.topHeaderBtn}`}
+              size="large"
+              variant="contained"
+            >
+              Create account
+            </Button>
           </Grid>
-        </form>
-      </Box>
+          <form onSubmit={handleLogin}>
+            <Grid className={classes.form}>
+              <Typography variant="h5" className={classes.headerText}>
+                Welcome back!
+              </Typography>
+              <Grid>
+                <FormControl
+                  margin="normal"
+                  required
+                  className={classes.formControl}
+                >
+                  <TextField
+                    fullWidth
+                    aria-label="username"
+                    label="Username"
+                    name="username"
+                    type="text"
+                    style={{ margin: 8, color: "#B0B0B0" }}
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                  />
+                </FormControl>
+              </Grid>
+              <Grid>
+                <FormControl
+                  margin="normal"
+                  className={classes.formControl}
+                  required
+                >
+                  <TextField
+                    fullWidth
+                    label="password"
+                    aria-label="password"
+                    type="password"
+                    name="password"
+                    style={{ margin: 8, color: "#B0B0B0" }}
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                  />
+                </FormControl>
+              </Grid>
+              <Grid>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  size="large"
+                  color="primary"
+                  className={`${classes.customButton} ${classes.CTABtn}`}
+                >
+                  Login
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
+        </Box>
+      </Grid>
     </Grid>
   );
 };
--- client/src/Signup.js
+++ client/src/Signup.js
@@ -7,13 +7,77 @@ import {
   Typography,
   Button,
   FormControl,
+  Hidden,
   TextField,
   FormHelperText,
+  makeStyles,
 } from "@material-ui/core";
 import { register } from "./store/utils/thunkCreators";
+import bgImage from "./assets/img/bg-img.png";
+import chatIcon from "./assets/img/bubble.svg";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    height: "100vh",
+  },
+  image: {
+    backgroundImage: `linear-gradient(to bottom,rgb(58, 141, 255,0.85), rgb(134, 185, 255,0.85)), url(${bgImage})`,
+    width: "100%",
+    height: "100vh",
+    backgroundSize: "cover",
+    color: "white",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+  },
+  headerText: {
+    marginBottom: "15px",
+    fontWeight: 600,
+  },
+  heroText: {
+    color: "#D8D8D8",
+    fontWeight: 400,
+  },
+  chatIcon: {
+    marginBottom: 30,
+  },
+  topHeader: {
+    alignItems: "center",
+    display: "flex",
+    justifyContent: "flex-end",
+    margin: "5%",
+  },
+  form: {
+    width: "60%",
+    maxWidth: "400px",
+    margin: "0px auto",
+    marginTop: "10rem",
+  },
+  formControl: {
+    width: "100%",
+  },
+  customButton: {
+    borderRadius: "3px",
+    boxShadow: "0px 2px 12px rgba(74, 106, 149, .2)",
+    fontSize: "0.9em",
+  },
+  topHeaderBtn: {
+    backgroundColor: "white",
+    color: "#3A8DFF",
+    marginLeft: "20px",
+    fontWeight: "400",
+  },
+  CTABtn: { display: "flex", margin: "40px auto", padding: "3% 15%" },
+  grayText: {
+    color: "#B0B0B0",
+    fontSize: "0.9em",
+  },
+}));
 
 const Login = (props) => {
   const history = useHistory();
+  const classes = useStyles();
   const { user, register } = props;
   const [formErrorMessage, setFormErrorMessage] = useState({});
 
@@ -37,72 +101,141 @@ const Login = (props) => {
   }
 
   return (
-    <Grid container justify="center">
-      <Box>
-        <Grid container item>
-          <Typography>Need to log in?</Typography>
-          <Button onClick={() => history.push("/login")}>Login</Button>
+    <Grid container className={classes.container}>
+      <Hidden only="xs">
+        <Grid item xs={false} sm={8} md={5} className={classes.image}>
+          <Box>
+            <img className={classes.chatIcon} src={chatIcon} alt="chat-icon" />
+            <Typography className={classes.heroText}>
+              Converse with anyone
+            </Typography>
+            <Typography className={classes.heroText}>
+              with any language
+            </Typography>
+          </Box>
         </Grid>
-        <form onSubmit={handleRegister}>
-          <Grid>
-            <Grid>
-              <FormControl>
-                <TextField
-                  aria-label="username"
-                  label="Username"
-                  name="username"
-                  type="text"
+      </Hidden>
+      <Grid item xs={12} sm={4} md={7}>
+        <Box>
+          <Grid className={classes.topHeader}>
+            <Typography className={classes.grayText}>
+              Already have an account?
+            </Typography>
+            <Button
+              className={`${classes.customButton} ${classes.topHeaderBtn}`}
+              size="large"
+              variant="contained"
+              onClick={() => history.push("/login")}
+            >
+              Login
+            </Button>
+          </Grid>
+          <form onSubmit={handleRegister}>
+            <Grid className={classes.form}>
+              <Typography variant="h5" className={classes.headerText}>
+                Create an account.
+              </Typography>
+              <Grid>
+                <FormControl
+                  margin="normal"
                   required
-                />
-              </FormControl>
-            </Grid>
-            <Grid>
-              <FormControl>
-                <TextField
-                  label="E-mail address"
-                  aria-label="e-mail address"
-                  type="email"
-                  name="email"
+                  className={classes.formControl}
+                >
+                  <TextField
+                    aria-label="username"
+                    label="Username"
+                    name="username"
+                    type="text"
+                    fullWidth
+                    required
+                    style={{ margin: 8, color: "#B0B0B0" }}
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                  />
+                </FormControl>
+              </Grid>
+              <Grid>
+                <FormControl
+                  margin="normal"
                   required
-                />
-              </FormControl>
-            </Grid>
-            <Grid>
-              <FormControl error={!!formErrorMessage.confirmPassword}>
+                  className={classes.formControl}
+                >
+                  <TextField
+                    label="E-mail address"
+                    aria-label="e-mail address"
+                    type="email"
+                    name="email"
+                    fullWidth
+                    style={{ margin: 8, color: "#B0B0B0" }}
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                    required
+                  />
+                </FormControl>
+              </Grid>
+              <FormControl
+                margin="normal"
+                required
+                className={classes.formControl}
+                error={!!formErrorMessage.confirmPassword}
+              >
                 <TextField
                   aria-label="password"
                   label="Password"
                   type="password"
                   inputProps={{ minLength: 6 }}
                   name="password"
+                  fullWidth
+                  style={{ margin: 8, color: "#B0B0B0" }}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
                   required
                 />
-                <FormHelperText>
+                <FormHelperText style={{ margin: 8 }}>
                   {formErrorMessage.confirmPassword}
                 </FormHelperText>
               </FormControl>
-            </Grid>
-            <Grid>
-              <FormControl error={!!formErrorMessage.confirmPassword}>
-                <TextField
-                  label="Confirm Password"
-                  aria-label="confirm password"
-                  type="password"
-                  inputProps={{ minLength: 6 }}
-                  name="confirmPassword"
+              <Grid>
+                <FormControl
+                  margin="normal"
                   required
-                />
-                <FormHelperText>
-                  {formErrorMessage.confirmPassword}
-                </FormHelperText>
-              </FormControl>
+                  className={classes.formControl}
+                  error={!!formErrorMessage.confirmPassword}
+                >
+                  <TextField
+                    label="Confirm Password"
+                    aria-label="confirm password"
+                    type="password"
+                    inputProps={{ minLength: 6 }}
+                    name="confirmPassword"
+                    fullWidth
+                    style={{ margin: 8, color: "#B0B0B0" }}
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                    required
+                  />
+                  <FormHelperText style={{ margin: 8 }}>
+                    {formErrorMessage.confirmPassword}
+                  </FormHelperText>
+                </FormControl>
+              </Grid>
+              <Button
+                type="submit"
+                variant="contained"
+                size="large"
+                color="primary"
+                className={`${classes.customButton} ${classes.CTABtn}`}
+              >
+                Create
+              </Button>
             </Grid>
-            <Button type="submit" variant="contained" size="large">
-              Create
-            </Button>
-          </Grid>
-        </form>
-      </Box>
+          </form>
+        </Box>
+      </Grid>
     </Grid>
   );
 };
