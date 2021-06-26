04910fe44f518907d5aa54f54e7f777824dca744
Connie Yu <connieyu18@yahoo.com>
2021-06-26 04:05:10 -0700

    added user class and ensure new message from sockets is sent to the right user

--- server/onlineUsers.js
+++ server/onlineUsers.js
@@ -1,2 +1,42 @@
-const onlineUsers = [];
+class OnlineUsers {
+  // Stores a dictionary of structure:
+  // {
+  //   userId :
+  //   {
+  //     socketId: int
+  //   }
+  // }
+  constructor() {
+    this.users = {};
+  }
+
+  // checks if userId exists
+  includes(userId) {
+    return userId in this.users;
+  }
+
+  // returns true if added, false if not added
+  addUser(userId, socketId) {
+    if (this.includes(userId)) return false;
+    this.users[userId] = {
+      socketId: socketId,
+    };
+    return true;
+  }
+
+  // returns the user structure
+  // null if it does not exist
+  getUser(userId) {
+    if (this.includes(userId)) {
+      return this.users[userId];
+    }
+    return null;
+  }
+
+  delete(userId) {
+    delete this.users[userId];
+  }
+}
+
+const onlineUsers = new OnlineUsers();
 module.exports = onlineUsers;
